808d24db1cb6012a09f804d4dbeeedfd775b898d
partsvaniaandro <ablabudaandro2@gmail.com>
2024-06-09 14:55:44 +0400

    day 9

---
+++ day 9/homework/homework0.py
@@ -0,0 +1,15 @@
+#asking the user for a first number
+num1 = float(input("please enter your first number: "))
+#asking the user for a second number
+num2 = float(input("please enter your second number: "))
+#asking the user for a third number
+num3 = float(input("please enter your third number: "))
+#asking the user for a fourth number
+num4 = float(input("please enter your fourth number: "))
+#asking the user for a fifth number
+num5 = float(input("please enter your fifth number: "))
+
+
+print(num1 + num2 - num3 * num4 / num5)
+
+
