1ba4a9dc1aa001edd6bc890581a1bf9c8f017a01
partsvaniaandro <ablabudaandro2@gmail.com>
2024-06-15 22:49:59 +0400

    GOA

---
+++ day 10/homework/homework1.py
@@ -0,0 +1,15 @@
+total=0
+for i in range(1, 101):
+    total+=i
+print(total)
+for i in range(1, 101):
+    total-=i
+print(total)
+for i in range(1, 101):
+    total*=i
+print(total)
+for i in range(1, 101):
+    total/=i
+print(total)
+
+    
