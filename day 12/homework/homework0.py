20beb1b55e1ae296cfb835e45f99f2647d3dcb07
partsvaniaandro <ablabudaandro2@gmail.com>
2024-06-21 19:35:15 +0400

    day 12

---
+++ day 12/homework/homework0.py
@@ -0,0 +1,28 @@
+i=0
+while i < 6:
+    print(i)
+    i = i + 1
+
+o=0
+while 0 < 6:
+    print(o)
+    if o==2:
+        break
+    o=o+1
+
+p=0
+while p < 6:
+    p=p+1
+    if p == 2:
+        continue
+    p=p+1
+    print(p)
+
+u=0
+while u < 6:
+    print(u)
+    u = u + 1
+else:
+    print("u is no longer less than 6")
+    
+
