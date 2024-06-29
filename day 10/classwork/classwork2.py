87668a0f26f6b678f1613f7197ff6d1d5b8b14be
partsvaniaandro <ablabudaandro2@gmail.com>
2024-06-29 18:37:45 +0400

    GOA

--- day 10/classwork/classwork2.py
+++ day 10/classwork/classwork2.py
@@ -1,4 +1,4 @@
 sum = 0
 for i in range(5, 16):
-    sum += i
+    sum += 1
 print(sum)
---
+++ day14/homework/homework0.py
@@ -0,0 +1,11 @@
+list=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list.pop())
+list2=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list2.pop(0))
+list3=["A", "B", "C", "D"]
+print(list3.pop(1))
+
+list4=[("mercedes", "lambo")("bugatti", "ferrari")]
+list4.pop()
+list4
+         
