93d8f35b042f246a2ce623a458d18fa9f5ca9642
partsvaniaandro <ablabudaandro2@gmail.com>
2024-07-26 17:37:34 +0400

    GOA

---
+++ day 18/homweork/homework.py
@@ -0,0 +1,15 @@
+person =  {
+    "name": "andria",
+    "age": "15",
+    "city": "tbilisi",
+    "work": "GOA mini leader"
+}
+print(person["name"] + " " + "is " + person["age"] + " " + "years old and works as an " + person["work"] + " " + "in " + person["city"])
+
+table = {
+    "mathematics": 9,
+    "english": 10,
+    "biology": 7,
+    "geography": 6
+}
+print((table["mathematics"]+table["english"]+table["biology"]+table["geography"])/4)
---
+++ day 22/classwork/classwork0.py
@@ -0,0 +1,7 @@
+def greet():
+    print("hello andria!")
+
+greet()
+greet()
+greet()
+greet()
