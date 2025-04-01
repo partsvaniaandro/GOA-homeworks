d01637694aebe7781461154400b71eec254a77f5
partsvaniaandro <ablabudaandro2@gmail.com>
2025-04-01 16:21:26 +0400

    GOA

--- day 91/homework/3/script.js
+++ day 91/homework/3/script.js
@@ -1,8 +1,18 @@
 let numInput = document.getElementById('numInput')
+let numInput1 = document.getElementById('numInput1')
+
 let addBtn = document.getElementById('addBtn')
+let addBtn1 = document.getElementById('addBtn1')
+
 let list = document.getElementById('list')
+let list1 = document.getElementById('list1')
+
 let warning = document.getElementById('warning')
+let warning1 = document.getElementById('warning1')
+
 let clearBtn = document.getElementById('clear')
+let clearBtn1 = document.getElementById('clear1')
+
 function higherOrLower(){
     if (numInput.value.length > 3){
         warning.textContent = 'input must not be longer than 3 characters'
@@ -26,3 +36,30 @@ function clearList(){
     list.innerHTML = ''
 }
 clearBtn.addEventListener('click', clearList)
+
+
+
+function endsWith(){
+    let color = document.createElement('div')
+    let lastChar = numInput1.value.slice(-1);
+    color.style.width = '50px'
+    color.style.height = '50px'
+    if (lastChar == '1'){
+        color.style.background = 'red'
+        list1.appendChild(color)
+    }
+    else if (lastChar == '2'){
+        color.style.background = 'green'
+        list1.appendChild(color)
+    }
+    else if (lastChar == '3'){
+        color.style.background = 'blue'
+        list1.appendChild(color)
+    }
+    numInput1.value = ''
+}
+addBtn1.addEventListener('click', endsWith)
+function clearList1(){
+    list1.innerHTML = ''
+}
+clearBtn1.addEventListener('click', clearList1)
