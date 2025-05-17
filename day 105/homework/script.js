bdb9b05a0dee1d230cfe06856b5f9a72a906c75f
partsvaniaandro <ablabudaandro2@gmail.com>
2025-05-17 09:34:17 +0400

    GOA

---
+++ day 105/homework/script.js
@@ -0,0 +1,21 @@
+let child = document.querySelector('.child')
+let parent = document.querySelector('.parent')
+let grandparent = document.querySelector('.grandparent')
+let colorInput = document.querySelector('.colorInput input')
+child.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    console.log(e.target.id)
+})
+parent.addEventListener('click', (e)=>{
+    console.log(e.target.id)
+})
+grandparent.addEventListener('click', (e)=>{
+    console.log(e.target.id)
+})
+colorInput.addEventListener('change', ()=>{
+    document.body.style.background = colorInput.value
+})
+let submit = document.getElementById('form')
+submit.addEventListener('submit', (e)=>{
+    e.preventDefault()
+})
