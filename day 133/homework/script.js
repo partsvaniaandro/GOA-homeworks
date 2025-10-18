1792ffa0c03104ee6aea58e6d7b8d816d59e8ea1
partsvaniaandro <ablabudaandro2@gmail.com>
2025-10-18 15:18:47 +0400

    GOA

--- day 133/homework/script.js
+++ day 133/homework/script.js
@@ -1,7 +1,7 @@
 import {weatherData} from "./city.js"
 let cityInput = document.getElementById('cityInput')
-let checkBtn = document.getElementById("checkBtn")
 let weatherOutput = document.getElementById('weatherInfo')
+let title = document.getElementById('title')
 
 function weatherDisplay(){
     weatherData.forEach(el => {
@@ -18,4 +18,43 @@ function weatherDisplay(){
     }
     })
 }
-checkBtn.addEventListener('click', weatherDisplay)
+cityInput.addEventListener('input', weatherDisplay)
+title.innerText = 'Hello, Kocho!'
+
+let colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+  "brown",
+  "black",
+  "white",
+  "gray",
+  "cyan",
+  "magenta",
+  "lime",
+  "teal",
+  "indigo",
+  "violet",
+  "gold",
+  "silver",
+  "beige"
+]
+let colorBtn = document.getElementById('colorBtn')
+function btnRandomColor(){
+    let randomIndex = Math.floor(Math.random() * colors.length)
+    let randomColor = colors[randomIndex]
+    colorBtn.style.background = randomColor
+}
+colorBtn.addEventListener('click', btnRandomColor)
+
+let userInput = document.getElementById('userInput')
+let userOutput = document.getElementById('userOutput')
+
+function inputToOutput(){
+    userOutput.innerText = userInput.value
+}
+userInput.addEventListener('input', inputToOutput)
