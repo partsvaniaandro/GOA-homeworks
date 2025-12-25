9331736ba4293e36bc92428b9f506b833876a673
partsvaniaandro <ablabudaandro2@gmail.com>
2025-12-25 22:48:51 +0400

    GOA

--- day 154/homework/vite-project/src/App.jsx
+++ day 154/homework/vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import Mount from "./components/mount"
 import Title from "./components/Title"
+import Count from "./components/Count"
 function App() {
   return (
     <>
       <Title />
       <Mount />
+      <Count />
     </>
   )
 }
