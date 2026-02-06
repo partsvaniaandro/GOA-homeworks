0cb164f03d250a52072f82c37c9c45a9b1ec55f6
partsvaniaandro <ablabudaandro2@gmail.com>
2026-02-06 21:02:57 +0400

    GOA

--- day 165/classwork/vite-project/src/App.jsx
+++ day 165/classwork/vite-project/src/App.jsx
@@ -1,25 +1,25 @@
-import { useReducer } from 'react'
-import {  } from 'react'
+// import { useReducer } from 'react'
+// import {  } from 'react'
 
-const reducer = (state, action) => {
-  switch(action.type){
-    case 'ADD_TASK':
-    DELETE_TASK
-    TOGGLE_TASK
-  }
-}
+// const reducer = (state, action) => {
+//   switch(action.type){
+//     case 'ADD_TASK':
+//     DELETE_TASK
+//     TOGGLE_TASK
+//   }
+// }
 
-function App() {
-  const [count, setCount] = useReducer((), [])
+// function App() {
+//   const [count, setCount] = useReducer((), [])
 
-  const handleSubmit = (e) => {
-    e.preventDefault
-  }
-  return (
-    <>
+//   const handleSubmit = (e) => {
+//     e.preventDefault
+//   }
+//   return (
+//     <>
       
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-export default App
+// export default App
